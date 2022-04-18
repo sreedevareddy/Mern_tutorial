e7cb27e3d5ed8bace86b24dc87ffcece1e2425a1
sreedeva <sreedevareddy.27@gmail.com>
2022-04-18 19:23:05 +0530

    commit

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -1,50 +1,22 @@
-import axios from "axios";
+import axios from 'axios'
 
-const API_URL = "/api/goals/";
+const API_URL = '/api/goals/'
 
 // Create NEW GOAL
 const createGoal = async (goalData, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
 
-  const response = await axios.post(API_URL, goalData, config);
+    const response = await axios.post(API_URL, goalData, config)
 
-  return response.data;
-};
-
-// delete NEW GOAL
-const deleteGoal = async (goalID, token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  const response = await axios.delete(API_URL + goalID, config);
-
-  return response.data;
-};
-
-// get user goals
-const getGoals = async (token) => {
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  };
-
-  const response = await axios.get(API_URL, config);
-
-  return response.data;
-};
+    return response.data
+}
 
 const goalService = {
-  createGoal,
-  getGoals,
-  deleteGoal
-};
+    createGoal
+}
 
 export default goalService;
